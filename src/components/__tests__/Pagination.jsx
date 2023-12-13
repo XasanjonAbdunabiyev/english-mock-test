86615dd0e2f0c0cafa26a6db4dd732ab02fa51f4
xasanjon <xasanjonabdunabiyev222@gmail.com>
2023-12-13 22:36:46 +0500

    added test pagination

--- src/components/__tests__/Pagination.jsx
+++ src/components/__tests__/Pagination.jsx
@@ -1,25 +1,49 @@
-import { useState } from 'react'
-import { Box, Button, ChakraProvider } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Box, Button } from '@chakra-ui/react'
 
-const itemsPerPage = 5
+const itemsPerPage = 5 // Number of items to display per page
 
-export function Pagination({ data }) {
+export const Pagination = ({ data }) => {
     const [currentPage, setCurrentPage] = useState(1)
 
+    // Calculate the range of items to display on the current page
     const indexOfLastItem = currentPage * itemsPerPage
-    const indexOfFirstItem = currentPage - itemsPerPage
-    const currentItems = data?.slice(indexOfFirstItem, indexOfLastItem)
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage
+    const currentItems = data.slice(indexOfFirstItem, indexOfLastItem)
+
+    // Change page
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber)
+    }
 
     // Generate page numbers
     const pageNumbers = Array.from(
         { length: Math.ceil(data.length / itemsPerPage) },
         (_, index) => index + 1
-    );
+    )
+
     return (
-        <>
-            <div>
-                <h3>Pagination</h3>
-            </div>
-        </>
+        <Box>
+            {/* Display current page items */}
+            <ul>
+                {currentItems.map((item, index) => (
+                    <li key={index}>{item.question}</li>
+                ))}
+            </ul>
+
+            {/* Display pagination */}
+            <Box>
+                {pageNumbers.map((number) => (
+                    <Button
+                        key={number}
+                        variant={number === currentPage ? 'solid' : 'outline'}
+                        onClick={() => handlePageChange(number)}
+                        mx={1}
+                    >
+                        {number}
+                    </Button>
+                ))}
+            </Box>
+        </Box>
     )
 }
--- src/components/__tests__/Test.jsx
+++ src/components/__tests__/Test.jsx
@@ -3,26 +3,42 @@ import { Pagination } from './Pagination'
 const questions = [
     {
         timeToThings: 5,
-        question: "what is your favourite color", 
-        timeToAnswer : 30
-    }, 
+        question: 'what is your favourite color',
+        timeToAnswer: 30,
+    },
     {
         timeToThings: 7,
-        question: "what is your full name", 
-        timeToAnswer : 30
-    }, 
+        question: 'what is your full name',
+        timeToAnswer: 30,
+    },
     {
         timeToThings: 7,
-        question: "what is your hobby", 
-        timeToAnswer : 30
-    }
+        question: 'what is your hobby',
+        timeToAnswer: 30,
+    },
+
+    {
+        timeToThings: 5,
+        question: 'what is your favourite color',
+        timeToAnswer: 30,
+    },
+    {
+        timeToThings: 7,
+        question: 'what is your full name',
+        timeToAnswer: 30,
+    },
+    {
+        timeToThings: 7,
+        question: 'what is your hobby',
+        timeToAnswer: 30,
+    },
 ]
 
 export const Test = () => {
     return (
-        <>
+        <div>
             <h1>My test component</h1>
-            <Pagination  data={questions}/>
-        </>
+            <Pagination data={questions} />
+        </div>
     )
 }
