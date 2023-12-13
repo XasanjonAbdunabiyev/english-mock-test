56d8f924075fb7c83e69c59eb514eca7178d089d
xasanjon <xasanjonabdunabiyev222@gmail.com>
2023-12-13 23:15:26 +0500

    added

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-import { Box, Center, Text, Link } from '@chakra-ui/react'
+import { Box, Text, Link } from '@chakra-ui/react'
 
 export const Footer = () => {
     return (
         <Box mt={2} borderTop="1px solid" py={4} as="footer" borderColor="gray">
-            <Text marginRight={4} my={3}>&copy; Copyright 2022</Text>
+            <Text marginRight={4} my={3}>
+                &copy; Copyright 2022
+            </Text>
             <div className="flex items-center justify-between max-[815px]:flex-col max-[815px]:items-start gap-5">
-                <div className='font-lg'>
+                <div className="font-lg">
                     <Link href="#" mx="2">
                         MultiLevel.uz
                     </Link>
@@ -24,10 +26,13 @@ export const Footer = () => {
                         MultiLevel.uz
                     </Link>
                     <Link href="#" mx="2">
-                        Services Licensed
+                        About Us
+                    </Link>
+                    <Link href="#" mx="2">
+                        Services
                     </Link>
                     <Link href="#" mx="2" marginRight={6}>
-                        № 154864
+                        Contact Us
                     </Link>
                 </div>
             </div>
